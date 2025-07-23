bb7afeebc7dc1e737c4a589efc8b876aadec9b0c
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-23 21:58:18 +0300

    Added DbContext and Installed Required Packages

---
+++ CylinderService/Data/CylinderDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CylinderService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CylinderService.Data
+{
+    public class CylinderDbContext : DbContext
+    {
+        public CylinderDbContext(DbContextOptions<CylinderDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Cylinder> Cylinders { get; set; } 
+       
+    }
+}
--- CylinderService/Models/Cylinder.cs
+++ CylinderService/Models/Cylinder.cs
@@ -2,5 +2,13 @@
 {
     public class Cylinder
     {
+        public Guid Id { get; set; }
+        public string? Size { get; set; }
+
+        public string Brand { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Condition { get; set; } = string.Empty;
+
+        public DateTime LastRefilled { get; set; } 
     }
 }
