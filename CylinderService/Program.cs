4275a1634ccc9904f122509fde2998e4915eaa0d
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-23 22:47:13 +0300

    Registerd in program.cs and added connection string

--- CylinderService/Program.cs
+++ CylinderService/Program.cs
@@ -1,6 +1,12 @@
+using CylinderService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Register DbContext
+builder.Services.AddDbContext<CylinderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
