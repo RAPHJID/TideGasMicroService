f297ed87a80f01443369beee583939b4c7fe9771
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-25 22:43:06 +0300

    Implemented CRUD functionality on my Service

---
+++ CylinderService/Services/CylinderService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using CylinderService.Data;
+using CylinderService.Models;
+using CylinderService.Models.DTOs;
+using CylinderService.Services.IServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace CylinderService.Services
+{
+    public class CylinderService : ICylinder
+    {
+        private readonly CylinderDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CylinderService(CylinderDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<IEnumerable<CylinderDto>> GetCylindersAsync()
+        {
+            var cylinders = await _context.Cylinders.ToListAsync();
+            return _mapper.Map<IEnumerable<CylinderDto>>(cylinders);
+        }
+
+        public async Task<CylinderDto?> GetCylinderByIdAsync(Guid id)
+        {
+            var cylinder = await _context.Cylinders.FirstOrDefaultAsync(c => c.Id == id);
+            if (cylinder == null) return null;
+            return _mapper.Map<CylinderDto>(cylinder);
+
+        }
+
+        public async Task<CylinderDto> AddCylinderAsync(AddUpdateCylinderDto cylinderDto)
+        {
+            var cylinder = _mapper.Map<Cylinder>(cylinderDto);
+            await _context.Cylinders.AddAsync(cylinder);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<CylinderDto>(cylinder);
+        }
+
+        public async Task<CylinderDto> UpdateCylinderAsync(Guid id, AddUpdateCylinderDto cylinderDto)
+        {
+            var cylinder = await _context.Cylinders.FindAsync(id);
+            _mapper.Map(cylinderDto, cylinder);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<CylinderDto>(cylinder);
+        }
+
+        public async Task<bool> DeleteCylinderAsync(Guid id)
+
+        {
+            var cylinder = await _context.Cylinders.FindAsync(id);
+            if (cylinder == null) return false;
+            _context.Cylinders.Remove(cylinder);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
+
+    }
+}
--- CylinderService/Services/IServices/ICylinder.cs
+++ CylinderService/Services/IServices/ICylinder.cs
@@ -1,6 +1,13 @@
-﻿namespace CylinderService.Services.IServices
+﻿using CylinderService.Models.DTOs;
+
+namespace CylinderService.Services.IServices
 {
     public interface ICylinder
     {
+        Task<IEnumerable<CylinderDto>> GetAllCylindersAsync();
+        Task<CylinderDto?> GetCylinderByIdAsync(Guid id);
+        Task<CylinderDto> AddCylinderAsync(AddUpdateCylinderDto cylinderDto);
+        Task<CylinderDto> UpdateCylinderAsync(Guid id, AddUpdateCylinderDto cylinderDto);
+        Task<bool> DeleteCylinderAsync(Guid id);
     }
 }
