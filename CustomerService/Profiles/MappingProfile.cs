323cc31d6b5a54adb5385073353b216330671366
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-28 16:21:42 +0300

    Added a mapping profile

---
+++ CustomerService/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CustomerService.Models;
+using CustomerService.Models.DTOs;
+
+namespace CustomerService.Profiles
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile()
+        {
+            // For creating, updating customer & also returning customer data
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+        }
+    }
+}
