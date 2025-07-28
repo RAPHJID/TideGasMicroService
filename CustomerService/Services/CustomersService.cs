5ac1b34374d3c3852af9cf7f0f52a439eae47a9e
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-28 21:39:34 +0300

    Add Customer Interface and service

---
+++ CustomerService/Services/CustomersService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CustomerService.Data;
+using CustomerService.Models;
+using CustomerService.Models.DTOs;
+using CustomerService.Services.IServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerService.Services
+{
+    public class CustomersService : ICustomer
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CustomersService(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return _mapper.Map<IEnumerable<CustomerDto>>(customers);
+        }
+        public async Task<CustomerDto?> GetCustomerByIdAsync(Guid customerId)
+        {
+            var customer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == customerId);
+            if (customer == null) return null;
+            return _mapper.Map<CustomerDto>(customer);
+        }
+        public async Task<CustomerDto> AddCustomerAsync(CustomerDto customerDto)
+        {
+            var customer = _mapper.Map<Customer>(customerDto);
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<CustomerDto>(customer);
+        }
+        public async Task<CustomerDto> UpdateCustomerAsync(Guid customerId, CustomerDto customerDto)
+        {
+            var customer = await _context.Customers.FindAsync(customerId);
+            if (customer == null) return null;
+            _mapper.Map(customerDto, customer);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<CustomerDto>(customer);
+        }
+        public async Task<bool> DeleteCustomerAsync(Guid customerId)
+        {
+            var customer = await _context.Customers.FindAsync(customerId);
+            if (customer == null) return false;
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ CustomerService/Services/IServices/ICustomer.cs
@@ -0,0 +1,15 @@
+﻿using CustomerService.Models.DTOs;
+
+namespace CustomerService.Services.IServices
+{
+    public interface ICustomer
+    {
+        Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+        Task<CustomerDto?> GetCustomerByIdAsync(Guid customerId);
+
+        Task<CustomerDto> AddCustomerAsync(CustomerDto customerDto);
+        Task<CustomerDto> UpdateCustomerAsync(Guid customerId, CustomerDto customerDto);
+
+        Task<bool> DeleteCustomerAsync(Guid customerId);
+    }
+}
