7ce608ff6e51bb05f5029be87a5fc8fa78e67e70
Jid Kimachia <kimachiajidraf@gmail.com>
2025-08-04 23:06:12 +0300

    Added Inventory Model

---
+++ InventoryService/Models/Inventory.cs
@@ -0,0 +1,18 @@
+﻿
+using CylinderService.Models;
+
+namespace InventoryService.Models
+{
+    public class Inventory
+    {
+        public Guid Id { get; set; }
+
+        public Guid CylinderId { get; set; }         
+        public Cylinder? Cylinder { get; set; }       
+
+        public decimal QuantityAvailable { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
