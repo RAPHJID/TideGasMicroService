903df957896e257c20c58abff96ae821e1eec2a7
Jid Kimachia <kimachiajidraf@gmail.com>
2025-08-05 18:10:36 +0300

    Added AppDbContext

---
+++ InventoryService/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Inventory> Inventorys { get; set; }
+    }
+}
