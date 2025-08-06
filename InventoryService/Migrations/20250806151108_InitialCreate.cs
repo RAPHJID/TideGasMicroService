12575501968ab6be6226af15dd9c8ee585536403
Jid Kimachia <kimachiajidraf@gmail.com>
2025-08-06 20:11:23 +0300

    Registered Db, Added Connection string and migrated

---
+++ InventoryService/Migrations/20250806151108_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cylinder",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Condition = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastRefilled = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cylinder", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventorys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CylinderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuantityAvailable = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventorys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inventorys_Cylinder_CylinderId",
+                        column: x => x.CylinderId,
+                        principalTable: "Cylinder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventorys_CylinderId",
+                table: "Inventorys",
+                column: "CylinderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventorys");
+
+            migrationBuilder.DropTable(
+                name: "Cylinder");
+        }
+    }
+}
