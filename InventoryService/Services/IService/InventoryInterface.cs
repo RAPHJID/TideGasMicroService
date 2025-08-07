2681ddfe81ce517ee42899958c3cf6fa6fdd9a76
Jid Kimachia <kimachiajidraf@gmail.com>
2025-08-07 12:33:38 +0300

    Added Inventory Service CRUD functionality

--- InventoryService/Services/IService/InventoryInterface.cs
+++ InventoryService/Services/IService/InventoryInterface.cs
@@ -8,8 +8,8 @@ namespace InventoryService.Services.IService
         Task <List<InventoryDto>> GetAllInventoriesAsync();
         Task<InventoryDto> GetInventoryByIdAsync(Guid inventoryId);
 
-        Task<InventoryDto> AddInventoryAsync(InventoryDto inventoryDto);
-        Task<InventoryDto> UpdateInventoryAsync(InventoryDto updatedInventory, Guid inventoryId);
+        Task<InventoryDto> AddInventoryAsync(AddUpdateInventory inventoryDto);
+        Task<InventoryDto> UpdateInventoryAsync(AddUpdateInventory updatedInventory, Guid inventoryId);
         Task <bool> DeletedInventoryAsync(Guid inventoryId);
 
     }
--- InventoryService/Services/InventoryService.cs
+++ InventoryService/Services/InventoryService.cs
@@ -1,6 +1,63 @@
-﻿namespace InventoryService.Services
+﻿using AutoMapper;
+using InventoryService.Data;
+using InventoryService.Models;
+using InventoryService.Models.DTOs;
+using InventoryService.Services.IService;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Services
 {
-    public class InventoryService
+    public class InventoryService : InventoryInterface
     {
+        private readonly AppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public InventoryService(AppDbContext appDb, IMapper mapper)
+        {
+            _appDbContext = appDb;
+            _mapper = mapper;
+        }
+
+
+        public async Task<InventoryDto> AddInventoryAsync(AddUpdateInventory inventoryDto)
+        {
+            var inventory = _mapper.Map<Inventory>(inventoryDto);
+            await _appDbContext.AddAsync(inventory);
+            await _appDbContext.SaveChangesAsync();
+            return _mapper.Map<InventoryDto>(inventory);
+        }
+
+
+        public async Task<bool> DeletedInventoryAsync(Guid inventoryId)
+        {
+            var inventory = await _appDbContext.Inventorys.FindAsync(inventoryId);
+            if (inventory == null) return false;
+            _appDbContext.Remove(inventory);
+            await _appDbContext.SaveChangesAsync(); 
+            return true;
+        }
+
+        public async Task<List<InventoryDto>> GetAllInventoriesAsync()
+        {
+            var inventories = await _appDbContext.Inventorys.ToListAsync();
+            return _mapper.Map<List<InventoryDto>>(inventories);
+        }
+
+        public async Task<InventoryDto> GetInventoryByIdAsync(Guid inventoryId)
+        {
+            var inventory = await _appDbContext.Inventorys.FirstOrDefaultAsync(i => i.Id == inventoryId);
+            if (inventory == null) return null;
+            return _mapper.Map<InventoryDto>(inventory);
+        }
+
+        public async Task<InventoryDto> UpdateInventoryAsync(AddUpdateInventory updatedInventory, Guid inventoryId)
+        {
+            var existing = await _appDbContext.Inventorys.FindAsync(inventoryId);
+            if (existing == null) return null;
+            _mapper.Map(updatedInventory, existing);
+            await _appDbContext.SaveChangesAsync();
+            return _mapper.Map<InventoryDto>(existing);
+
+        }
     }
 }
