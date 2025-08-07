fbfdae87718130d1844e85de5c877ed9b2f4c34c
Jid Kimachia <kimachiajidraf@gmail.com>
2025-08-07 12:04:37 +0300

    Added Inventory Interface

---
+++ InventoryService/Services/IService/InventoryInterface.cs
@@ -0,0 +1,16 @@
+﻿using InventoryService.Models;
+using InventoryService.Models.DTOs;
+
+namespace InventoryService.Services.IService
+{
+    public interface InventoryInterface
+    {
+        Task <List<InventoryDto>> GetAllInventoriesAsync();
+        Task<InventoryDto> GetInventoryByIdAsync(Guid inventoryId);
+
+        Task<InventoryDto> AddInventoryAsync(InventoryDto inventoryDto);
+        Task<InventoryDto> UpdateInventoryAsync(InventoryDto updatedInventory, Guid inventoryId);
+        Task <bool> DeletedInventoryAsync(Guid inventoryId);
+
+    }
+}
