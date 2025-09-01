0b463bcabe6853d23af2da973763f0f4ad9816aa
Jid Kimachia <kimachiajidraf@gmail.com>
2025-09-01 23:10:51 +0300

    Added my DTOs For GET, POST and PUT

---
+++ TransactionService/Models/DTOs/CreateUpdateTransactionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TransactionService.Models.DTOs
+{
+    public class CreateUpdateTransactionDTO
+    {
+        public Guid CylinderId { get; set; }  
+        public int Quantity { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
---
+++ TransactionService/Models/DTOs/TransactionResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TransactionService.Models.DTOs
+{
+    public class TransactionResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string? CylinderName { get; set; }   
+        public int Quantity { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
