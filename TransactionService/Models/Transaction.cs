0e4bb7ddde6dd662ddde81aeb9f99d1d86577af7
Jid Kimachia <kimachiajidraf@gmail.com>
2025-09-01 23:04:49 +0300

    Added Transaction Model

---
+++ TransactionService/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace TransactionService.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public Guid CylinderId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
