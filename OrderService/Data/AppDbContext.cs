564800e51a5b96c9e9ad59b022c98f763b324b31
Jid Kimachia <kimachiajidraf@gmail.com>
2025-10-14 08:22:18 +0300

    Added AppDbContext

---
+++ OrderService/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+namespace OrderService.Data
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+            public DbSet<Order> Orders { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                //Store enums as strings in the DB
+                modelBuilder.Entity<Order>()
+                    .Property(o => o.Status)
+                    .HasConversion<string>();
+
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+
+
+
