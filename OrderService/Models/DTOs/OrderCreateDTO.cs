1c17e45c2b5bbedf39d3f62a1033d02991779722
Jid Kimachia <kimachiajidraf@gmail.com>
2025-10-14 19:14:18 +0300

    feat : Created DTOs

---
+++ OrderService/Models/DTOs/OrderCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderService.Models.DTOs
+{
+    public class OrderCreateDTO
+    {
+        public Guid CustomerId { get; set; }
+        public Guid CylinderId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
---
+++ OrderService/Models/DTOs/OrderReadDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Models.DTOs
+{
+    public class OrderReadDTO
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid CylinderId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string? Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
