08a00f2df4c41adeee49fc316c4cd89f9d9371fd
Jid Kimachia <kimachiajidraf@gmail.com>
2025-10-31 22:20:08 +0300

    Added HttpClient that lets  OrdersService call the InventoryService API

---
+++ OrderService/Services/HttpClients/InventoryApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OrdersService.Services.HttpClients
+{
+    public interface IInventoryApiClient
+    {
+        Task<bool> CheckStockAsync(Guid cylinderId, int quantity);
+    }
+
+    public class InventoryApiClient : IInventoryApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public InventoryApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> CheckStockAsync(Guid cylinderId, int quantity)
+        {
+            // Example: GET https://localhost:7037/api/inventory/check/{cylinderId}?quantity=5
+            var response = await _httpClient.GetAsync($"api/inventory/check/{cylinderId}?quantity={quantity}");
+
+            if (!response.IsSuccessStatusCode)
+                return false;
+
+            var result = await response.Content.ReadFromJsonAsync<bool>();
+            return result;
+        }
+    }
+}
+
