d7616e1c0afe92cd695d801a3a0311c9a4fbc9fd
Jid Kimachia <kimachiajidraf@gmail.com>
2025-11-04 21:19:46 +0300

    feat : Created a CustomerApiClient in OrdersService

---
+++ OrderService/Clients/CustomerApiClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System;
+using OrderService.Models;
+
+namespace OrderService.Clients
+{
+    public class CustomerApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Customer?> GetCustomerByIdAsync(Guid customerId)
+        {
+            var response = await _httpClient.GetAsync($"/api/customers/{customerId}");
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadFromJsonAsync<Customer>();
+        }
+    }
+}
