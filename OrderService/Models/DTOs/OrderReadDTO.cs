d00e48588155417bdde3497ced1c61a01c692e06
Jid Kimachia <kimachiajidraf@gmail.com>
2025-11-14 18:48:10 +0300

    Updated OrderReadDto

--- OrderService/Models/DTOs/OrderReadDTO.cs
+++ OrderService/Models/DTOs/OrderReadDTO.cs
@@ -5,6 +5,9 @@
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
         public Guid CylinderId { get; set; }
+        public string CustomerName { get; set; }
+        public string CylinderName { get; set; }
+
         public int Quantity { get; set; }
         public decimal TotalPrice { get; set; }
         public string? Status { get; set; }
