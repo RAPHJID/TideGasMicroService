0efbff7902807d189f9b41d65e1e87f0f76cbb12
Jid Kimachia <kimachiajidraf@gmail.com>
2025-11-29 15:53:36 +0300

    updated my inventory interface

--- InventoryService/Services/IService/InventoryInterface.cs
+++ InventoryService/Services/IService/InventoryInterface.cs
@@ -1,28 +1,14 @@
 ﻿using InventoryService.Models.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace InventoryService.Services.IService;
 
 public interface InventoryInterface
 {
-    // Inventory methods
-    Task<List<InventoryDto>> GetAllInventoriesAsync();
-    Task<ServiceResult<InventoryDto>> GetInventoryByIdAsync(Guid inventoryId);
-    Task<ServiceResult<InventoryDto>> AddInventoryAsync(AddUpdateInventory inventoryDto);
-    Task<ServiceResult<InventoryDto>> UpdateInventoryAsync(AddUpdateInventory updatedInventory, Guid inventoryId);
+    Task<IEnumerable<InventoryDto>> GetAllInventoriesAsync();
+    Task<InventoryDto?> GetInventoryByIdAsync(Guid cylinderId);
+    Task AddInventoryAsync(AddUpdateInventory dto);
+    Task IncreaseQuantityAsync(Guid cylinderId, int quantity);
+    Task DecreaseQuantityAsync(Guid cylinderId, int quantity);
+    Task DeleteInventoryAsync(Guid cylinderId);
     Task<bool> CheckStockAsync(Guid cylinderId, int quantity);
-
-    Task DecreaseQuantityAsync(Guid id, int quantity);
-    Task IncreaseQuantityAsync(Guid id, int quantity);
-
-    Task<ServiceResult<bool>> DeletedInventoryAsync(Guid inventoryId);
-
-    // Cylinder proxy methods
-    Task<IEnumerable<CylinderDto>> GetCylindersAsync();
-    Task<CylinderDto?> GetCylinderByIdAsync(Guid id);
-    Task<CylinderDto> CreateCylinderAsync(AddUpdateCylinderDto dto);
-    Task<CylinderDto?> UpdateCylinderAsync(Guid id, AddUpdateCylinderDto dto);
-    Task<bool> DeleteCylinderAsync(Guid id);
 }
