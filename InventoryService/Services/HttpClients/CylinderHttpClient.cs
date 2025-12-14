f348e0d2c8f1cefbf1bcd9becafd650876649648
Jid Kimachia <kimachiajidraf@gmail.com>
2025-12-14 14:21:46 +0300

    Updated CylinderHttpClient and its interface to handle errors

--- InventoryService/Services/HttpClients/CylinderHttpClient.cs
+++ InventoryService/Services/HttpClients/CylinderHttpClient.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using InventoryService.Common;
+using InventoryService.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using InventoryService.Models.DTOs;
 
 namespace InventoryService.Services.HttpClients
 {
@@ -35,18 +36,29 @@ namespace InventoryService.Services.HttpClients
                    ?? Array.Empty<CylinderDto>();
         }
 
-        public async Task<CylinderDto?> GetByIdAsync(Guid id)
+        public async Task<Result<CylinderDto>> GetByIdAsync(Guid id)
         {
             var resp = await _client.GetAsync($"api/Cylinder/{id}");
 
             if (resp.StatusCode == HttpStatusCode.NotFound)
-                return null;
+                return Result<CylinderDto>.Failure("Cylinder not found");
 
             if (!resp.IsSuccessStatusCode)
-                await HandleNonSuccessAsync(resp);
+            {
+                var body = await resp.Content.ReadAsStringAsync();
+                return Result<CylinderDto>.Failure(
+                    $"Cylinder API error {(int)resp.StatusCode}: {body}"
+                );
+            }
 
-            return await resp.Content.ReadFromJsonAsync<CylinderDto>();
+            var cylinder = await resp.Content.ReadFromJsonAsync<CylinderDto>();
+
+            if (cylinder == null)
+                return Result<CylinderDto>.Failure("Empty response from CylinderService");
+
+            return Result<CylinderDto>.Success(cylinder);
         }
+
 
 
         public async Task<CylinderDto> CreateAsync(AddUpdateCylinderDto dto)
--- InventoryService/Services/HttpClients/ICylinderHttpClient.cs
+++ InventoryService/Services/HttpClients/ICylinderHttpClient.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using InventoryService.Common;
+using InventoryService.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using InventoryService.Models.DTOs;
 
 namespace InventoryService.Services.HttpClients
 {
     public interface ICylinderHttpClient
     {
         Task<IEnumerable<CylinderDto>> GetAllAsync();
-        Task<CylinderDto?> GetByIdAsync(Guid id);
+        Task<Result<CylinderDto>> GetByIdAsync(Guid id);
         Task<CylinderDto> CreateAsync(AddUpdateCylinderDto dto);
         Task<CylinderDto?> UpdateAsync(Guid id, AddUpdateCylinderDto dto);
         Task<bool> DeleteAsync(Guid id);
