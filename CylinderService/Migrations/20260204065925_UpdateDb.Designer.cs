9a45577f4a9e365269f445c2f5b87fd2a30a32b3
Jid Kimachia <kimachiajidraf@gmail.com>
2026-02-04 10:00:40 +0300

    Added migrations and updated Db

--- CylinderService/Migrations/20260204065925_UpdateDb.Designer.cs
+++ CylinderService/Migrations/20260204065925_UpdateDb.Designer.cs
@@ -4,6 +4,7 @@ using CylinderService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CylinderService.Migrations
 {
     [DbContext(typeof(CylinderDbContext))]
-    partial class CylinderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260204065925_UpdateDb")]
+    partial class UpdateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CylinderService/Migrations/20260204065925_UpdateDb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CylinderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedAt",
+                table: "Cylinders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedByStaffId",
+                table: "Cylinders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoldToday",
+                table: "Cylinders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalStock",
+                table: "Cylinders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedAt",
+                table: "Cylinders");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedByStaffId",
+                table: "Cylinders");
+
+            migrationBuilder.DropColumn(
+                name: "SoldToday",
+                table: "Cylinders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalStock",
+                table: "Cylinders");
+        }
+    }
+}
