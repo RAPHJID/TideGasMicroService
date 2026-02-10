cbee0fb0f86d073a2d0413755bbc9093c99ec578
Jid Kimachia <kimachiajidraf@gmail.com>
2026-02-10 10:17:31 +0300

    updated model to add who updated the inventory

--- InventoryService/Models/Inventory.cs
+++ InventoryService/Models/Inventory.cs
@@ -1,16 +1,21 @@
 ﻿
-using CylinderService.Models;
-
 namespace InventoryService.Models
 {
     public class Inventory
     {
         public Guid Id { get; set; }
 
+        // Link to CylinderService
         public Guid CylinderId { get; set; }
-        public decimal QuantityAvailable { get; set; }
+
+        // Current stock
+        public int QuantityAvailable { get; set; }
+
+        // Audit fields
         public DateTime LastUpdated { get; set; }
 
-
+        // WHO updated inventory (staff/admin user id from JWT)
+        public string UpdatedByUserId { get; set; } = string.Empty;
     }
 }
+
