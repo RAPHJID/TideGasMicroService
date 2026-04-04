9265b9009a5e9ffa63008f81469922b0fe480204
Jid Kimachia <kimachiajidraf@gmail.com>
2026-04-04 20:35:31 +0300

    Migrated, Updated Db and deleted unnecccessary files

---
+++ CylinderService/Migrations/20260403200918_AddImageUrlToCylinder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CylinderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToCylinder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Cylinders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Cylinders");
+        }
+    }
+}
