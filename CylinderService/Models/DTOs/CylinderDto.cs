cbc10c9884b13c16a9e91b3b4b42930f761864fc
Jid Kimachia <kimachiajidraf@gmail.com>
2026-04-04 21:07:32 +0300

    updated my dto to add some fields in my ui

--- CylinderService/Models/DTOs/CylinderDto.cs
+++ CylinderService/Models/DTOs/CylinderDto.cs
@@ -5,10 +5,11 @@
         public Guid Id { get; set; }
         public string? Size { get; set; }
         public string? ImageUrl { get; set; }
-
         public string Brand { get; set; } = string.Empty;
         public string Status { get; set; } = string.Empty;
         public string Condition { get; set; } = string.Empty;
-
+        public int SoldToday { get; set; }        
+        public int TotalStock { get; set; }        
+        public DateTime? LastUpdatedAt { get; set; } 
     }
 }
