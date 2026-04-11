74ee9e5a362027505b72a4d8497ba2577fb3819b
Jid Kimachia <kimachiajidraf@gmail.com>
2026-04-11 21:28:17 +0300

    Migrated and updated my db

---
+++ CylinderService/Migrations/20260411182624_RemoveImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CylinderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Cylinders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Cylinders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
